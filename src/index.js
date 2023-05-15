b2869a4c52c6650ff9e21e5469181a7b47068956
jjg2362 <wornjs2362@gmail.com>
2023-05-15 16:27:04 +0900

    [hotfix]index 파일 prettier 적용

--- src/index.js
+++ src/index.js
@@ -11,11 +11,11 @@ import { BrowserRouter } from 'react-router-dom';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-      <GlobalStyles />
-      <ThemeProvider theme={theme}>
-        <BrowserRouter>
-          <App />
-        </BrowserRouter>
+    <GlobalStyles />
+    <ThemeProvider theme={theme}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </ThemeProvider>
   </React.StrictMode>
 );
